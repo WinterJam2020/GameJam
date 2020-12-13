fcd37f779f2c176bba167e414339eb0caab4be47
HowManySmall <lhamil29@bruinmail.slcc.edu>
2020-12-12 20:53:35 -0700

    typed

--- src/ServerStorage/Repository/Server/CatmullRomSpline.lua
+++ src/ServerStorage/Repository/Server/CatmullRomSpline.lua
@@ -26,8 +26,16 @@ Example code:
 
 --]]
 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Resources = require(ReplicatedStorage.Resources)
+local Typer = Resources:LoadLibrary("Typer")
+
 local Spline = {ClassName = "Spline"}
 Spline.__index = Spline
+
+local ConstrainedAlpha = {Alpha = function(Value, TypeOfString)
+	return TypeOfString == "number" and Value >= 0 and Value <= 1
+end}
 
 do
 	-- solve for the v3 position along the spline
@@ -44,49 +52,49 @@ do
 	local RIEMANN_STEP = 0.05 -- Smaller steps produce more accurate results
 
 	-- Creates a segment of a spline through p1 and p2 using control points p0 and p3
-	function Spline.new(p0, p1, p2, p3)
+	Spline.new = Typer.AssignSignature(Typer.Vector3, Typer.Vector3, Typer.Vector3, Typer.Vector3, function(P0, P1, P2, P3)
 		local cA = table.create(3)
-		cA[1], cA[2], cA[3] = 2*p1.X, 2*p1.Y, 2*p1.Z
+		cA[1], cA[2], cA[3] = 2*P1.X, 2*P1.Y, 2*P1.Z
 
 		local cB = table.create(3)
-		cB[1], cB[2], cB[3] = -p0.X + p2.X, -p0.Y + p2.Y, -p0.Z + p2.Z
+		cB[1], cB[2], cB[3] = -P0.X + P2.X, -P0.Y + P2.Y, -P0.Z + P2.Z
 
 		local cC = table.create(3)
-		cC[1], cC[2], cC[3] = 2*p0.X - 5*p1.X + 4*p2.X - p3.X, 2*p0.Y - 5*p1.Y + 4*p2.Y - p3.Y, 2*p0.Z - 5*p1.Z + 4*p2.Z - p3.Z
+		cC[1], cC[2], cC[3] = 2*P0.X - 5*P1.X + 4*P2.X - P3.X, 2*P0.Y - 5*P1.Y + 4*P2.Y - P3.Y, 2*P0.Z - 5*P1.Z + 4*P2.Z - P3.Z
 
 		local cD = table.create(3)
-		cD[1], cD[2], cD[3] = -p0.X + 3*p1.X - 3*p2.X + p3.X, -p0.Y + 3*p1.Y - 3*p2.Y + p3.Y, -p0.Z + 3*p1.Z - 3*p2.Z + p3.Z
+		cD[1], cD[2], cD[3] = -P0.X + 3*P1.X - 3*P2.X + P3.X, -P0.Y + 3*P1.Y - 3*P2.Y + P3.Y, -P0.Z + 3*P1.Z - 3*P2.Z + P3.Z
 
 		return setmetatable({
 			cA = cA;
 			cB = cB;
 			cC = cC;
 			cD = cD;
-			length = (function()
-				local result = 0
-				local start_here = solve(cA, cB, cC, cD, 0)
-				for i = 0, 1 - RIEMANN_STEP, RIEMANN_STEP do
-					local stop_here = solve(cA, cB, cC, cD, i + RIEMANN_STEP)
-					result += (stop_here - start_here).Magnitude
-					start_here = stop_here
+			Length = (function()
+				local Result = 0
+				local StartHere = solve(cA, cB, cC, cD, 0)
+				for Index = 0, 1 - RIEMANN_STEP, RIEMANN_STEP do
+					local StopHere = solve(cA, cB, cC, cD, Index + RIEMANN_STEP)
+					Result += (StopHere - StartHere).Magnitude
+					StartHere = StopHere
 				end
 
-				result *= RIEMANN_STEP
-				return result
+				Result *= RIEMANN_STEP
+				return Result
 			end)();
 		}, Spline)
-	end
+	end)
 
 	-- Find a Vector3 position on the spline
-	function Spline:Solve(t)
-		return solve(self.cA, self.cB, self.cC, self.cD, t)
-	end
+	Spline.Solve = Typer.AssignSignature(2, ConstrainedAlpha, function(self, Alpha: number): Vector3
+		return solve(self.cA, self.cB, self.cC, self.cD, Alpha)
+	end)
 
 	-- Find a CFrame position & orientation on the spline rotated in the direction of the path.  No z rotation.
 	-- A seperate step for rotation might be better
-	function Spline:SolveNorm(t, smoothing)
-		return CFrame.new(self:Solve(t), self:Solve(t + (smoothing or 0.01)))
-	end
+	Spline.SolveNorm = Typer.AssignSignature(2, ConstrainedAlpha, Typer.OptionalNumber, function(self, Alpha: number, Smoothing: number?): CFrame
+		return CFrame.new(self:Solve(Alpha), self:Solve(Alpha + (Smoothing or 0.01)))
+	end)
 end
 
 -- Piecewise spline
@@ -95,78 +103,71 @@ end
 local Path = {ClassName = "Path"}
 Path.__index = Path
 
-function Path.new(pts)
-	assert(#pts >= 4, "At least four points are required to construct a spline path")
-	local parts = {}
-	local length = 0
-	local prev_a, prev_b = nil, nil
-	local num_points = #pts
-	local loops = pts[1] == pts[num_points]
+type Array<Value> = {[number]: Value}
+
+Path.new = Typer.AssignSignature(Typer.ArrayOfVector3s, function(Points: Array<Vector3>)
+	local NumberOfPoints = #Points
+	assert(NumberOfPoints >= 4, "At least four points are required to construct a spline path")
+	local Parts = {}
+	local Length = 0
+	local PreviousA, PreviousB = nil, nil
+	local Loops = Points[1] == Points[NumberOfPoints]
 
 	-- catmull rom splines don't pass through all four points, so extrapolate some extras as control points
-	local sa
-	if loops then
-		sa = Spline.new(pts[2]:Lerp(pts[1], 2), pts[1], pts[2], pts[3])
-	else
-		sa = Spline.new(pts[num_points], pts[1], pts[2], pts[3])
-	end
+	local SplineValue =
+		Loops and Spline.new(Points[2]:Lerp(Points[1], 2), Points[1], Points[2], Points[3]) or
+		Spline.new(Points[NumberOfPoints], Points[1], Points[2], Points[3])
 
-	length += sa.length
-	parts[1] = sa
-	for i, v in ipairs(pts) do
-		local nxt = pts[i + 1]
-		if prev_a and prev_b and nxt then
-			local n = Spline.new(prev_a, prev_b, v, nxt)
-			length += n.length
-			parts[#parts + 1] = n
+	Length += SplineValue.Length
+	Parts[1] = SplineValue
+
+	for Index, Value in ipairs(Points) do
+		local Next = Points[Index + 1]
+		if PreviousA and PreviousB and Next then
+			local NewSpline = Spline.new(PreviousA, PreviousB, Value, Next)
+			Length += NewSpline.Length
+			Parts[#Parts + 1] = NewSpline
 		end
 
-		prev_a = prev_b
-		prev_b = v
+		PreviousA = PreviousB
+		PreviousB = Value
 	end
 
-	do
-		local sb
-		if loops then
-			sb = Spline.new(pts[num_points - 2], pts[num_points - 1], pts[num_points], pts[num_points - 1]:Lerp(pts[num_points], 2))
-		else
-			sb = Spline.new(pts[num_points - 2], pts[num_points - 1], pts[num_points], pts[1])
-		end
+	local SplineB =
+		Loops and Spline.new(Points[NumberOfPoints - 2], Points[NumberOfPoints - 1], Points[NumberOfPoints], Points[NumberOfPoints - 1]:Lerp(Points[NumberOfPoints], 2)) or
+		Spline.new(Points[NumberOfPoints - 2], Points[NumberOfPoints - 1], Points[NumberOfPoints], Points[1])
 
-		length += sb.length
-		parts[#parts + 1] = sb
+	Length += SplineB.Length
+	Parts[#Parts + 1] = SplineB
+
+	local Ranges = table.create(#Parts)
+	local CSum = 0
+	for Index, Part in ipairs(Parts) do
+		CSum += Part.Length
+		Ranges[Index] = CSum / Length
 	end
 
-	local ranges = table.create(#parts)
-	do
-		local csum = 0
-		for i, part in ipairs(parts) do
-			csum += part.length
-			ranges[i] = csum / length
-		end
-	end
-
-	local nr = #ranges
+	local RangeLength = #Ranges
 	--Accepts a number in [0, 1] representing a point on the path.
-	local function GetPointOnPath(val)
-		local res, af
-		for i = 1, nr do
-			if ranges[i] > val or i == nr then
-				af = (val - (ranges[i - 1] or 0))/parts[i].length*length
-				res = parts[i]
+	local function GetPointOnPath(Value)
+		local SplinePoint, Alpha
+		for Index = 1, RangeLength do
+			if Ranges[Index] > Value or Index == RangeLength then
+				Alpha = (Value - (Ranges[Index - 1] or 0)) / Parts[Index].Length * Length
+				SplinePoint = Parts[Index]
 				break
 			end
 		end
 
-		return res:Solve(af)
+		return SplinePoint:Solve(Alpha)
 	end
 
 	return setmetatable({
-		parts = parts;
-		length = length;
+		Parts = Parts;
+		Length = Length;
 		GetPointOnPath = GetPointOnPath;
 	}, Path)
-end
+end)
 
 return {
 	Spline = Spline;
