27307adfb4e10e3cd29c156c10570a7b46ecfe95
EthanCurtiss <ethanhcurtiss@gmail.com>
2020-12-29 15:16:52 -0500

    updated

--- src/ServerScriptService/ServerTest.server.lua
+++ src/ServerScriptService/ServerTest.server.lua
@@ -4,21 +4,13 @@ local GenerateSkiPath = Resources:LoadLibrary("GenerateSkiPath")
 local GenerateMarkers = Resources:LoadLibrary("GenerateMarkers")
 local GenerateGates = Resources:LoadLibrary("GenerateGates")
 
-local Spline = GenerateSkiPath()
-GenerateMarkers(Spline)
-GenerateGates(Spline)
+local SplineRemote = ReplicatedStorage.SplineRemote
 
--- local SplineCFrame0 = Spline.GetCFrameOnPath(0)
--- workspace.CurrentCamera.CFrame = SplineCFrame0 + Vector3.new(0, 20, 0)
--- workspace.CurrentCamera.Focus = SplineCFrame0 + SplineCFrame0.LookVector * 20
+workspace.Baseplate:Destroy()
+local SkiPath, SkiPathCFrames, CameraPathCFrames = GenerateSkiPath()
+GenerateMarkers(SkiPath)
+GenerateGates(SkiPath)
 
-repeat
-	wait()
-until #game.Players:GetPlayers() > 0
-
-for _, plr in ipairs(game.Players:GetPlayers()) do
-	plr:LoadCharacter()
-	local chr = plr.Character
-	local root = chr:WaitForChild("HumanoidRootPart")
-	root.CFrame = Spline.GetCFrameOnPath(0.01) + Vector3.new(0, 20, 0)
+function SplineRemote.OnServerInvoke()
+	return SkiPathCFrames, CameraPathCFrames
 end
